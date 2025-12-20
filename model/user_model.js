3264033b72332daf3e9328524d59b284f946b10e
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-12-20 18:05:21 +0500

    Added User Schema Model

---
+++ model/user_model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose'); // import mongoose package
+const database = require('../config/database'); // import database connectivity
+
+// create user Schema 
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    email:{
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+    },
+    passwoord:{
+        type: String,
+        required: true
+    }
+});
+
+
+const UserModel = database.model('user', userSchema);
+
+module.exports = UserModel;
