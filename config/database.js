5308c41b37b70e2f4fa1049ebd479bb94ce069c8
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-12-20 17:54:13 +0500

    Setup with MongoDB

---
+++ config/database.js
@@ -0,0 +1,9 @@
+const mangoose = require('mongoose');
+
+const connection = mangoose.createConnection('mongodb://127.0.0.1:27017').on('open', ()=> {
+    console.log("MongoDB Connected");
+}).on('error', ()=> {
+    console.log("MongoDB connection error");
+});
+
+module.exports = connection;
