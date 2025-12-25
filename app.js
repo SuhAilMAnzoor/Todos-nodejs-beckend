a97835f8588efc838f987086b56140c224c481ad
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-12-25 18:26:38 +0500

    Initialize Express app, enable JSON parsing, and mount user routes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userRouter = require('./routers/user_router');
-
+//
 const app = express();
 
 app.use(express.json());
@@ -8,9 +8,3 @@ app.use(express.json());
 app.use('/', userRouter);
 
 module.exports = app;
-
-// // in app.js temporarily
-// app.post('/test', (req, res) => {
-//     console.log(req.body);
-//     res.send(req.body);
-// });
