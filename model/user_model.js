500fc3fbb95d097d0a05ca625731e7d1fdc5f7ea
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-12-25 19:13:18 +0500

    Store the hash password in database to make the user secure using bycrpt

--- model/user_model.js
+++ model/user_model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose'); // import mongoose package
 const database = require('../config/database'); // import database connectivity
+const bcrypt = require("bcrypt");
 
 // create user Schema 
 const { Schema } = mongoose;
@@ -16,6 +17,19 @@ const userSchema = new Schema({
         required: true
     }
 });
+
+userSchema.pre('save', async function(){
+    try {
+        var user = this;
+        const salt = await(bcrypt.genSalt(10));
+        const hashpass = await bcrypt.hash(user.password, salt);
+
+        user.password = hashpass;
+        
+    } catch (error) {
+        
+    }
+})
 
                                 //collection name is 'users'
 const UserModel = database.model('users', userSchema);
