b64c8a2df8afe683b2a30e1e31f2738b7472b12b
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-12-25 18:20:55 +0500

    Create user router to handle registration API and load controller

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const userRouter = require('./routers/user_router');
 
 const app = express();
 
+app.use(express.json());
+
+app.use('/', userRouter);
+
 module.exports = app;
+
+// // in app.js temporarily
+// app.post('/test', (req, res) => {
+//     console.log(req.body);
+//     res.send(req.body);
+// });
